c64e03e611503a5d80366499620d8c64670a4ac7
MazenHassan10 <mazen0000072@gmail.com>
2024-12-26 15:19:00 +0200

    api is working

---
+++ script.js
@@ -0,0 +1,23 @@
+const unorderedList = document.getElementById("unorderedList");
+async function GetPhotos() {
+  const Photos = await fetch("https://api.unsplash.com/photos", {
+    headers: {
+      Authorization: "Client-ID sL4fK8qH8iKKf1yaKGIm8Bgj_ZRDa4rHnswwASCEEqU",
+    },
+  });
+  const data = await Photos.json();
+  return data;
+}
+
+async function showPhotos() {
+  const photos = await GetPhotos();
+  photos.forEach((element) => {
+    console.log(element.urls.regular);
+    const listItem = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = element.urls.small;
+    listItem.append(img);
+    unorderedList.append(listItem);
+  });
+}
+showPhotos();
